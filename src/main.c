8b1a4ce0493f17184e20823a7e41bef2f5e1c341
slush <hubert02twa@gmail.com>
2022-06-10 19:11:29 +0200

    First commit

---
+++ src/main.c
@@ -0,0 +1,35 @@
+#include <X11/Xlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+   Display *d;
+   Window w;
+   XEvent e;
+   int s;
+ 
+   d = XOpenDisplay(NULL);
+   if (d == NULL) {
+      fprintf(stderr, "Cannot open display\n");
+      exit(1);
+   }
+ 
+   s = DefaultScreen(d);
+   w = XCreateSimpleWindow(d, RootWindow(d, s), 10, 10, 100, 100, 1,
+                           BlackPixel(d, s), WhitePixel(d, s));
+   XSelectInput(d, w, ExposureMask | KeyPressMask);
+   XMapWindow(d, w);
+ 
+   while (1) {
+      XNextEvent(d, &e);
+      if (e.type == Expose) {
+          
+      }
+      if (e.type == KeyPress)
+         break;
+   }
+ 
+   XCloseDisplay(d);
+   return 0;
+}
